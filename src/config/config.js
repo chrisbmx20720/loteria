1d25e8ff74d4186ab66f95143d799dd35c150da4
chrisbmx20720 <christian.al.ac20@gmail.com>
2025-05-04 22:19:39 -0600

    Mee

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const ApiUrl = 'https://calculadoraapi-f86y.onrender.com/api/';
+export const ApiUrl = 'https://calculadoraapi-f86y.onrender.com/api';
 
 //export const ApiUrl = "http://localhost:3000/api"
